62c88c06ac86d5b66b5fa807893c4a5f8dec481e
Ruhan van der Berg <35257374+RuhanSA079@users.noreply.github.com>
2025-09-10 17:03:12 +0200

    Create radxa-cm3-io.sh

---
+++ config/boards/radxa-cm3-io.sh
@@ -0,0 +1,10 @@
+# shellcheck shell=bash
+
+export BOARD_NAME="Radxa CM3 IO Board"
+export BOARD_MAKER="Radxa"
+export BOARD_SOC="Rockchip RK3566"
+export BOARD_CPU="ARM Cortex A55"
+export UBOOT_PACKAGE="u-boot-radxa-rk3588"
+export UBOOT_RULES_TARGET="radxa-cm3io-rk3566"
+export COMPATIBLE_SUITES=("jammy")
+export COMPATIBLE_FLAVORS=("server")
